dde2c394f82939c748b8c8558612481c06d2100d
alemata <amataloni@gmail.com>
2015-06-29 20:03:38 -0300

    finish Node expression

--- tp2/expressions.py
+++ tp2/expressions.py
@@ -9,7 +9,6 @@ class Initial(object):
   def children(self):
     return self.items
 
-
 class Node(object):
 
   def __init__(self, label, items):
@@ -20,51 +19,13 @@ class Node(object):
     return str(self.label)
 
   def _element(self, x):
-    if(x.__class__ == Element or x.__class__ == Node):
+    if(x.__class__ == Element or x.__class__ == Node or x.__class__ == Number):
       return x
     else:
       return Element(x)
 
   def children(self):
     return map(self._element, self.items)
-
-
-
-
-class Tempo(object):
-
-  def __init__(self, figure, number):
-    self.figure = figure
-    self.number = number
-
-  def name(self):
-    return "te"
-
-  def children(self):
-    return [ Element('tempo'), Element(self.figure), Number(self.number)]
-
-class Compass(object):
-
-  def __init__(self, value1, value2):
-    self.value1 = value1
-    self.value2 = value2
-
-  def name(self):
-    return "compass: " + str(self.value1) + "/" + str(self.value2)
-
-  def children(self):
-    return []
-
-class Vars(object):
-
-  def __init__(self, items):
-    self.items = items
-
-  def name(self):
-    return "emancu"
-
-  def children(self):
-    return self.items
 
 class Element(object):
   def __init__(self, value):
--- tp2/parser_rules.py
+++ tp2/parser_rules.py
@@ -1,6 +1,6 @@
 from lexer_rules import tokens
 
-from expressions import Initial, Tempo, Compass, Number, Vars, Node
+from expressions import Initial, Node, Element, Number
 
 def p_expression_initial(subexpressions):
   'expression : te co vars'
@@ -10,49 +10,25 @@ def p_expression_tempo(subexpressions):
   'te : TEMPO FIGURE NUMBER'
   subexpressions[0] = Node('te', subexpressions[1:])
 
-
-
 def p_expression_compass(subexpressions):
   'co : COMPASS_V NUMBER DIV NUMBER'
-  subexpressions[0] = Compass(subexpressions[2], subexpressions[4])
+  subexpressions[0] = Node('co', subexpressions[1:])
 
 def p_vars(subexpressions):
-  'vars : CONST NAME EQUAL cons_val SEMICOLON'
+  'vars : CONST NAME EQUAL cons_val SEMICOLON vars'
 
-  subexpressions[0] = Vars(subexpressions[2::2])
+  subexpressions[0] = Node('vars', subexpressions[1:])
 
 def p_vars_empty(subexpressions):
   'vars :'
 
-  subexpressions[0] = Number('819247')
-
-def p_cons_val(s):
+def p_cons_val_number(s):
   'cons_val : NUMBER'
-  '         | NAME'
   s[0] = Number(s[1])
 
-
-def p_vars_lambda(subexpressions):
-  'vars : CONST NAME EQUAL cons_val'
-
-def p_term_times(subexpressions):
-  'term : term TIMES factor'
-  subexpressions[0] = Multiplication(subexpressions[1], subexpressions[3])
-
-
-def p_term_factor(subexpressions):
-  'term : factor'
-  subexpressions[0] = subexpressions[1]
-
-
-def p_factor_number(subexpressions):
-  'factor : NUMBER'
-  subexpressions[0] = Number(subexpressions[1])
-
-
-def p_factor_expression(subexpressions):
-  'factor : LPAREN expression RPAREN'
-  subexpressions[0] = subexpressions[2]
+def p_cons_val_name(s):
+  'cons_val : NAME'
+  s[0] = Element(s[1])
 
 def p_error(subexpressions):
   import pdb; pdb.set_trace()
